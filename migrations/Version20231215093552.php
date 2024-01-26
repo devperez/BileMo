96128852c88e7340dac9f361f68ea1e81508b073
DavidUMC <david.perez@upmycard.com>
2024-01-26 15:14:00 +0100

    some more php doc

--- migrations/Version20231215093552.php
+++ migrations/Version20231215093552.php
@@ -8,27 +8,40 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * This migration creates the necessary database tables for the customer, phone, and user entities.
  */
 final class Version20231215093552 extends AbstractMigration
 {
+    /**
+     * Gets the description of the migration.
+     *
+     * @return string The migration description.
+     */
     public function getDescription(): string
     {
-        return '';
+        return 'Auto-generated Migration for creating customer, phone, and user tables.';
     }
 
+    /**
+     * Executes the "up" migration.
+     *
+     * @param Schema $schema The schema object.
+     */
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE customer (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', name VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_81398E09E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE phone (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', brand VARCHAR(255) NOT NULL, model VARCHAR(255) NOT NULL, price VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, screen_size VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', customer_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, user_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, INDEX IDX_8D93D6499395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6499395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
     }
 
+    /**
+     * Executes the "down" migration.
+     *
+     * @param Schema $schema The schema object.
+     */
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6499395C3F3');
         $this->addSql('DROP TABLE customer');
         $this->addSql('DROP TABLE phone');
